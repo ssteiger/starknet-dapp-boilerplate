912f7297af0a6630e5355b3a75d6d722fe7dd9fd
Sören Steiger <soren1@gmx.net>
2022-07-07 11:54:56 +0200

    Add starknet specific helpers

---
+++ packages/react-app/src/helpers/starknet/index.js
@@ -0,0 +1,69 @@
+import starknet, {
+  Contract,
+  Account,
+  defaultProvider,
+  ec,
+  encode,
+  hash,
+  json,
+  number,
+  stark,
+  shortString,
+  uint256,
+  Abi,
+} from "starknet";
+import { getStarknet } from "get-starknet";
+// https://github.com/0xs34n/starknet.js/blob/develop/src/utils/shortString.ts
+import { encodeShortString, decodeShortString } from "starknet/dist/utils/shortString";
+// https://github.com/0xs34n/starknet.js/blob/develop/src/utils/number.ts
+import { toFelt, toBN } from "starknet/dist/utils/number";
+import { utils } from "ethers";
+import BN, { isBN } from "bn.js";
+
+export const createContract = (abi, address) => {
+  console.log({ abi });
+  const provider = getStarknet().provider;
+  return new Contract(abi, address, provider);
+};
+
+export const callContract = async (contract, method, ...args) => {
+  try {
+    return await contract.call(method, args);
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+};
+
+export const sendTransaction = async (contract, method, args = {}) => {
+  try {
+    const calldata = stark.compileCalldata(args);
+    const transaction = {
+      contractAddress: contract.address,
+      entrypoint: method,
+      calldata,
+    };
+    return await getStarknet().account.execute(transaction);
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+};
+
+const getUint256CalldataFromBN = bn => {
+  return {
+    type: "struct",
+    ...uint256.bnToUint256(bn),
+  };
+};
+
+export const parseInputAmountToUint256 = (input, _decimals) => {
+  let decimals;
+  if (!_decimals) {
+    decimals = 18;
+  } else {
+    decimals = _decimals;
+  }
+
+  const uint256Calldata = getUint256CalldataFromBN(utils.parseUnits(input, decimals).toString());
+
+  return uint256Calldata;
+};
