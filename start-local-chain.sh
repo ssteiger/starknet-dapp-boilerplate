246a3dffd859bdde710118eaf3b8bbac181510c4
Sören Steiger <soren1@gmx.net>
2022-07-27 16:53:27 +0200

    Move start local chain commands into bash script

---
+++ start-local-chain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'create and enter the virtual environment'
+python3 -m venv ~/cairo_venv
+source ~/cairo_venv/bin/activate
+echo 'create and enter the virtual environment: OK'
+
+echo 'install requirements'
+pip install -r requirements.txt
+echo 'install requirements: OK'
