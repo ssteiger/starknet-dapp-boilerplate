c2ac0f837b99765367e41aef0db80ddfceacf5fb
Sören Steiger <soren1@gmx.net>
2022-07-27 15:16:34 +0200

    Update Account component

--- packages/react-app/src/components/Account.jsx
+++ packages/react-app/src/components/Account.jsx
@@ -9,73 +9,53 @@ import WalletModal from "./WalletModal";
 
 import { useStarknet, InjectedConnector } from "@starknet-react/core";
 
-export function ConnectWallet() {}
+// export function ConnectWallet() {}
 
-export default function Account() {
+export default function Account({ blockExplorer }) {
   const { account, connect, disconnect, connectors } = useStarknet();
 
-  const injected = useMemo(() => new InjectedConnector(), []);
+  // const injected = useMemo(() => new InjectedConnector(), []);
 
-  //const etherscanLink = blockExplorerLink(address, blockExplorer);
-
-  /*
-  if (account) {
-    return (
-      <div>
-        <p>Account: {account}</p>
-        <button onClick={() => disconnect()}>Disconnect</button>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      {connectors.map((connector, idx) => (
-        <button key={idx} onClick={() => connect(connector)}>
-          Connect
-        </button>
-      ))}
-    </>
-  );
-  */
+  const voyagerLink = blockExplorerLink(account, blockExplorer);
 
   const connectWallet = (
     <div className="flex items-center">
       <Menu as="div" className="ml-3 relative">
         {connectors.map(connector => {
-          console.log({ connector });
-          return connector.available() ? (
-            <button
-              key={connector?.id()}
-              onClick={() => connect(connector)}
-              className="inline-flex items-center px-3 py-0.5 rounded-full text-base font-normal bg-blue-100 hover:bg-blue-200 text-gray-800 dark:bg-gray-900 dark:text-white"
-            >
-              connect {/*connector.name()*/}
-            </button>
-          ) : null;
+          if (connector.available()) {
+            return (
+              <button
+                key={connector?.id()}
+                onClick={() => connect(connector)}
+                className="inline-flex items-center px-3 py-0.5 rounded-full text-base font-normal bg-blue-100 hover:bg-blue-200 text-gray-800 dark:bg-gray-900 dark:text-white"
+              >
+                connect {/*connector.name()*/}
+              </button>
+            );
+          }
+          return null;
         })}
       </Menu>
     </div>
   );
 
   const accountNavigation = [
-    { name: "Wallet", action: () => console.log("TOOD: now show wallet") },
+    { name: "View on Explorer", action: () => window.open(voyagerLink, "_blank").focus() },
     { name: "Disconnect", action: () => disconnect() },
-    //{ name: 'View on Explorer', action: () => window.open(etherscanLink, '_blank').focus() },
   ];
 
   const accountMenu = (
     <Menu as="div" className="ml-3 relative">
       <div className="flex items-center inline-flex items-center pl-3.5 border border-transparent select-none text-sm text-gray-900 leading-4 font-normal rounded-full shadow-sm bg-slate-200 dark:bg-neutral-900 dark:text-white">
-        {/* TODO: */}
+        {/* TODO: fetch correct balance */}
         <Balance address={account} provider={null} price={0} textSize="text-lg" />
         <Menu.Button className="inline-flex items-center px-3.5 py-1.5 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm bg-slate-100 hover:border-slate-400 focus:outline-none focus:border-slate-400 dark:bg-neutral-800 dark:hover:border-gray-700 dark:focus:border-gray-700">
           <span className="sr-only">Open user menu</span>
           <Address
             address={account}
-            disableAddressLink={false}
+            disableAddressLink={true}
+            blockExplorer={blockExplorer}
             //ensProvider={mainnetProvider}
-            //blockExplorer={blockExplorer}
           />
         </Menu.Button>
       </div>
