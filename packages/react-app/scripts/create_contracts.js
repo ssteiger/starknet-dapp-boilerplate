b57d99b1a85377cf60d503b62187fe87e07d25e6
Sören Steiger <soren1@gmx.net>
2022-07-31 17:54:25 +0200

    React app: Create hardhat contract json files if not present

--- packages/react-app/scripts/create_contracts.js
+++ packages/react-app/scripts/create_contracts.js
@@ -1,10 +1,20 @@
 const fs = require("fs");
 
+// create hardhat_contracts.json
 if (!fs.existsSync("./src/contracts/hardhat_contracts.json")) {
   try {
     fs.writeFileSync("./src/contracts/hardhat_contracts.json", JSON.stringify({}));
-
     console.log("src/contracts/hardhat_contracts.json created.");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// create hardhat_starknet_contracts.json
+if (!fs.existsSync("./src/contracts/hardhat_starknet_contracts.json")) {
+  try {
+    fs.writeFileSync("./src/contracts/hardhat_starknet_contracts.json", JSON.stringify({}));
+    console.log("src/contracts/hardhat_starknet_contracts.json created.");
   } catch (error) {
     console.log(error);
   }
